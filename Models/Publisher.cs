99088bf9f6b5e954c3c50209777eb3476242434d
TUDOR <TUDOR@TUDOR>
2022-10-30 17:08:30 +0200

    Add project files.

---
+++ Models/Publisher.cs
@@ -0,0 +1,8 @@
+﻿namespace Andriesei_Tudor_Lab2.Models
+{
+    public class Publisher
+    {public int ID { get; set; }
+     public string PublisherName { get; set; }
+     public ICollection<Book>? Books { get; set; } //navigation property
+    }
+}
